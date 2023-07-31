0467eac71241e5ed78f6dd691f24cbe375d7d840
geniushyeon <dev.geniushyeon@gmail.com>
2023-07-31 21:05:24 +0900

    🎉 project init

---
+++ src/main/java/com/meowhanyang/MeowhanyangApplication.java
@@ -0,0 +1,13 @@
+package com.meowhanyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowhanyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowhanyangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meowhanyang/MeowhanyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meowhanyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowhanyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
