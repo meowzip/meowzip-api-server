2e060e54ab6f04080ba5a55e7c286677317f313a
geniushyeon <dev.geniushyeon@gmail.com>
2023-10-28 14:55:58 +0900

    🔧 메인 클래스에 `EnableJpaAuditing` 어노테이션 추가

--- meowzip-api-server/src/main/java/com/meowzip/MeowzipApplication.java
+++ meowzip-api-server/src/main/java/com/meowzip/MeowzipApplication.java
@@ -2,8 +2,10 @@ package com.meowzip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MeowzipApplication {
 
 	public static void main(String[] args) {
