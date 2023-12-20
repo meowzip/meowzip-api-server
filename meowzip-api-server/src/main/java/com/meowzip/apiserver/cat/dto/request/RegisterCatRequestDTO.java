5661e47b304e873821c1bcc6063f47a01b90de33
geniushyeon <dev.geniushyeon@gmail.com>
2023-12-20 17:10:25 +0900

    📝 swagger documentation

--- meowzip-api-server/src/main/java/com/meowzip/apiserver/cat/dto/request/RegisterCatRequestDTO.java
+++ meowzip-api-server/src/main/java/com/meowzip/apiserver/cat/dto/request/RegisterCatRequestDTO.java
@@ -3,15 +3,27 @@ package com.meowzip.apiserver.cat.dto.request;
 import com.meowzip.cat.entity.Cat;
 import com.meowzip.cat.entity.Sex;
 import com.meowzip.member.entity.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Schema
 public record RegisterCatRequestDTO(
+
+        @Schema(description = "고양이 이름", example = "냥이")
         String name,
+
+        @Schema(description = "고양이 성별", implementation = Sex.class)
         Sex sex,
+
+        @Schema(description = "중성화 여부")
         boolean isNeutered,
+
+        @Schema(description = "만난 날짜")
         LocalDate metAt,
+
+        @Schema(description = "메모")
         String memo
 ) {
 
--- meowzip-api-server/src/main/java/com/meowzip/apiserver/cat/dto/response/CatResponseDTO.java
+++ meowzip-api-server/src/main/java/com/meowzip/apiserver/cat/dto/response/CatResponseDTO.java
@@ -1,11 +1,21 @@
 package com.meowzip.apiserver.cat.dto.response;
 
 import com.meowzip.cat.entity.Cat;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema
 public record CatResponseDTO(
+
+        @Schema(description = "고양이 고유키")
         Long id,
+
+        @Schema(description = "고양이 이미지 URL")
         String imageUrl,
+
+        @Schema(description = "고양이 이름")
         String name,
+
+        @Schema(description = "공동냥육 여부")
         boolean isCoParented
 ) {
 
