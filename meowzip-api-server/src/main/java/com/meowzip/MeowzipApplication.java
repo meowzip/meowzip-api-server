922a167fbe0a36df9f960562f8b8ad449dce397f
geniushyeon <dev.geniushyeon@gmail.com>
2024-02-25 23:26:48 +0900

    🐛 swagger execute cors 해결

--- meowzip-api-server/src/main/java/com/meowzip/MeowzipApplication.java
+++ meowzip-api-server/src/main/java/com/meowzip/MeowzipApplication.java
@@ -1,5 +1,7 @@
 package com.meowzip;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @EnableCaching
 @EnableFeignClients
+@OpenAPIDefinition(servers = {@Server(url = "https://dev-server.meowzip.com", description = "개발 서버")})
 @SpringBootApplication
 public class MeowzipApplication {
 
