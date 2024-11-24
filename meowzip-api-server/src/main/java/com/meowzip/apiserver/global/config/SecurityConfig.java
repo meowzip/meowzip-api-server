78eb5d47fb3a46dc3bf240cf1716defa412f0b0b
geniushyeon <dev.geniushyeon@gmail.com>
2024-11-24 23:51:12 +0900

    🐛 공동냥육 상세 조회 API - 신청자 프로필 사진 추가

--- meowzip-api-server/src/main/java/com/meowzip/apiserver/global/config/SecurityConfig.java
+++ meowzip-api-server/src/main/java/com/meowzip/apiserver/global/config/SecurityConfig.java
@@ -22,7 +22,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
