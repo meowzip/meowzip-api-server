7e5e4ee7e643f53c0f954c73839b1b54803de22a
geniushyeon <dev.geniushyeon@gmail.com>
2024-12-12 21:25:03 +0900

    ✨ 푸시 알림 수신 여부 on/off api 추가

--- meowzip-api-server/src/main/java/com/meowzip/apiserver/member/swagger/MemberSwagger.java
+++ meowzip-api-server/src/main/java/com/meowzip/apiserver/member/swagger/MemberSwagger.java
@@ -44,4 +44,7 @@ public interface MemberSwagger {
 
     @Operation(summary = "회원 삭제, 추후 탈퇴로 변경 예정")
     CommonResponse<Void> withdraw(@Parameter(hidden = true) Principal principal);
+
+    @Operation(summary = "알림 수신 여부 on/off")
+    CommonResponse<Void> toggleNotificationReceive(Principal principal);
 }
